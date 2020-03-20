c0d155dde3a11547f733cf68336a9d8b27156085
Justin Gaba <gabalicious113@gmail.com>
2020-03-20 16:07:39 -0400

    Add parcel api

---
+++ server/parcel-runner.js
@@ -0,0 +1,44 @@
+const watch = require('node-watch');
+const execa = require('execa');
+const Path = require('path');
+
+const parcelBundler = Path.join(
+  __dirname,
+  '..',
+  'server',
+  'parcel-bundler.js',
+);
+
+const parcelConfig = Path.join(
+  __dirname,
+  'config',
+  'parcel-config.json',
+);
+
+let parcelRun = async () => {
+  const subprocess = execa('node', [parcelBundler]);
+  await subprocess.stdout.pipe(process.stdout);
+
+  try {
+    watch(
+      [parcelBundler, parcelConfig],
+      { recursive: true },
+      function(event) {
+        if (event === 'update') {
+          console.log('\nRestarting Parcel Bundler\n');
+          subprocess.kill('SIGTERM', {
+            forceKillAfterTimeout: 2000,
+          });
+          parcelRun();
+          return false;
+        }
+      },
+    );
+  } catch (error) {
+    if (subprocess.killed || error.isCanceled) {
+      console.log(`subprocess.killed ${subprocess.killed}`); // true
+      console.log(`error.isCanceled ${error.isCanceled}`); // true
+    }
+  }
+};
+parcelRun();
